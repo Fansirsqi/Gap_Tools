11f02add24e7445690436cec308320706c60c761
Byseven <38542005+Fansirsqi@users.noreply.github.com>
2023-07-10 11:13:15 +0800

    pandas 做gap尝试

--- gap_tool.py
+++ gap_tool.py
@@ -1,36 +1,28 @@
 # _*_coding:utf-8_*_
-
+import os
 import pandas as pd
 from tqdm import tqdm
 import pandas as pd
-from openpyxl import load_workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
 
-def set_brand1_true(path):
-    # path = './植村秀-6.1-6.30品牌数据Gap.xlsx'
+
+def set_brand_true(path,brand_name='brand'):
     with pd.ExcelFile(path) as excel_file:
-        brand = excel_file.parse('brand')
-        _t = brand.iloc[0].tolist()  # 获取第二行数据
+        brand = excel_file.parse(f'{brand_name}')
+        _t = brand.iloc[0].tolist()  # 获取第二行数据,表头默认为索引
         system = excel_file.parse('system')
         _t1 = system.columns.to_list()
-        column_mapping = {col: _index for _index, col in enumerate(_t, start=0)}
-        # 创建 brand1 表
         brand1 = pd.DataFrame(columns=_t1)
-        for tcol in _t1:
+        column_mapping = {col: _index for _index, col in enumerate(_t, start=0)}
+        for tcol in _t1:# 将TS表头匹配品牌表头，如果有值，就拿到列名称
             if tcol in column_mapping:
-                # 从 brand 取出对应列数据，并存入 brand1
-                brand_col_data = brand.iloc[:, column_mapping[tcol]].tolist()
-                # brand1[col] = 
-                # print(column_mapping[tcol],tcol,len(brand_col_data))
-                for i in tqdm(range(len(brand_col_data)),desc=f'{tcol}列',ncols=120):
-                    _target = f'={system}!{tcol}{i}'
-                    brand1.at[i+1,tcol] = _target
-        # 保存到新表 brand1
+                _brand_index = column_mapping[tcol] #对应列在品牌表中的索引  
+                for i in range(brand.shape[0]):
+                    relative_reference = f"='{brand_name}'!R{i+1}C{_brand_index+1}"  # 相对单元格引用
+                    brand1.at[i, tcol] = relative_reference
         with pd.ExcelWriter(path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
             brand1.to_excel(writer, sheet_name='brand1', index=False)
     print("源文件保存成功！")
 
 
 path = './植村秀-6.1-6.30品牌数据Gap.xlsx'
-
-set_brand1_true(path)
+# set_brand1_true(path)
