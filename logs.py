bfd967da0f07183b055e4354da33543d0e93fdb3
fansir <2104898527@qq.com>
2023-09-19 15:15:12 +0800

    feat: ✨引入全新的日志系统

---
+++ logs.py
@@ -0,0 +1,17 @@
+from sys import stdout
+
+from loguru import logger
+
+logger.remove()
+logger.add(
+    stdout,
+    # encoding='utf-8',
+    colorize=True,
+    format="<g>{time:MM-DD HH:mm:ss}</g> <level><w>[</w>{level}<w>]</w></level> | {message}",
+)
+
+logger.add(
+    "log.logs",
+    encoding="utf-8",
+    format="<g>{time:MM-DD HH:mm:ss}</g> <level><w>[</w>{level}<w>]</w></level> | {message}",
+)
