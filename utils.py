3a879bf1ad7d36c02c3d5e57dc4ecb6acb95b5d0
fansir <2104898527@qq.com>
2024-02-21 18:30:09 +0800

    :art: 格式化工具类

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-import os
 import glob
+import os
 import platform
 
 
@@ -20,7 +20,7 @@ class DotDict(dict):
         return value
 
 
-def list_folder_(filter=[".venv", "__pycache__", ".vscode", ".git", ".idea", "export_人群", "export_日报"], prt=True) -> list:
+def list_folder_(filter=['.venv', '__pycache__', '.vscode', '.git', '.idea', 'export_人群', 'export_日报'], prt=True) -> list:
     """列出当前项目文件夹
 
     Args:
@@ -30,19 +30,19 @@ def list_folder_(filter=[".venv", "__pycache__", ".vscode", ".git", ".idea", "ex
     Returns:
         list: _description_
     """
-    folder_list = [name for name in os.listdir(".") if os.path.isdir(name) and name not in filter]
+    folder_list = [name for name in os.listdir('.') if os.path.isdir(name) and name not in filter]
     if len(folder_list) != 0:
         if prt is True:
             count = 1
-            print("=" * 30, "文件夹列表", "=" * 30)
+            print('=' * 30, '文件夹列表', '=' * 30)
             for i in folder_list:
-                print(f"     {count}.{i}")
+                print(f'     {count}.{i}')
                 count += 1
-            print("=" * 68)
+            print('=' * 68)
     return folder_list
 
 
-def list_files(filter="*.xlsx", prt=True, prefix="~$", path=None):
+def list_files(filter='*.xlsx', prt=True, prefix='~$', path=None):
     """列出文件列表
 
     Args:
@@ -69,17 +69,17 @@ def list_files(filter="*.xlsx", prt=True, prefix="~$", path=None):
     if prt is True:
         # 打印文件列表
         count = 1
-        print("=" * 30, "文件列表", "=" * 30)
+        print('=' * 30, '文件列表', '=' * 30)
         for file in files:
             file_name = os.path.basename(file)
-            print(f" {count}. {file_name}")
+            print(f' {count}. {file_name}')
             count += 1
-        print("=" * 68)
+        print('=' * 68)
     return files
 
 
 def get_file_list(dir):
-    search_path = os.path.join(dir, "*")
+    search_path = os.path.join(dir, '*')
     # 构建要匹配的文件路径模式
     files = glob.glob(search_path)
     return files
@@ -89,22 +89,22 @@ def clear():
     """清屏"""
     # 返回系统平台/OS的名称，如Linux，Windows，Java，Darwin
     system = platform.system()
-    if system == "Windows":
-        os.system("cls")
+    if system == 'Windows':
+        os.system('cls')
     else:
-        os.system("clear")
+        os.system('clear')
 
 
-def input_selector(text="请输选择：") -> str:
+def input_selector(text='请输选择：') -> str:
     """获取输入信息"""
     try:
-        user_input = input(f"\033[1;33m{text}\033[0m")
+        user_input = input(f'\033[1;33m{text}\033[0m')
         if user_input:
-            return user_input.split(" ")
+            return user_input.split(' ')
         else:
             return []
     except KeyboardInterrupt as e:
-        print(f"\n手动终止！{e}")
+        print(f'\n手动终止！{e}')
         exit(0)
 
 
@@ -123,7 +123,7 @@ def file_selector():
         current = file_list[int(select) - 1]
         return current
     except Exception as e1:
-        print(f"输入有误！{e1},请重试！")
+        print(f'输入有误！{e1},请重试！')
         return file_selector()
 
 
@@ -132,13 +132,13 @@ def set_az():
     生成一个列表[A-ZZ]
     @return: [A-ZZ]
     """
-    sr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    sr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     sl = [char for char in sr]
     sl += [char1 + char2 for char1 in sr for char2 in sr]
     # print(len(sl))
     return sl
 
 
-if __name__ == "__main__":
-    files: list = list_files(path=r"C:\Users\HPLaptop-14S\Documents\Gap文档\1", filter="*.csv")
+if __name__ == '__main__':
+    files: list = list_files(path=r'C:\Users\HPLaptop-14S\Documents\Gap文档\1', filter='*.csv')
     print(files)
